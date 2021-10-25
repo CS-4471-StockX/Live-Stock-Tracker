95aa0f97dbcc0a16e99e3f842236750cf6fe4dbe
wanger26 <jwanger@uwo.ca>
2021-10-25 09:41:51 -0400

    Inital Upload

---
+++ src/main/java/com/stockx/livestocktracker/LiveStockTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.stockx.livestocktracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStockTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStockTrackerApplication.class, args);
+	}
+
+}
