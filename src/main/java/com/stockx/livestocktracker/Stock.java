32471cf2d507c2be37bc280af12f73b16ad412c3
wanger26 <jwanger@uwo.ca>
2021-10-25 14:59:17 -0400

    Replaced retrival service since it did not provide value to system with direct call to external api

--- src/main/java/com/stockx/livestocktracker/Stock.java
+++ src/main/java/com/stockx/livestocktracker/Stock.java
@@ -1,22 +1,30 @@
 package com.stockx.livestocktracker;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import lombok.Data;
 
 @Data
 public class Stock {
 
+    @JsonAlias("c")
     private Double currentPrice;
 
+    @JsonAlias("d")
     private Double priceChange;
 
+    @JsonAlias("dp")
     private Double percentageChange;
 
+    @JsonAlias("h")
     private Double dayHigh;
 
+    @JsonAlias("l")
     private Double dayLow;
 
+    @JsonAlias("o")
     private Double openingPrice;
 
+    @JsonAlias("pc")
     private Double previousClosingPrice;
 
 }
--- src/main/java/com/stockx/livestocktracker/configurations/AmazonSNSConfiguration.java
+++ src/main/java/com/stockx/livestocktracker/configurations/AmazonSNSConfiguration.java
@@ -1,4 +1,4 @@
-package com.stockx.livestocktracker;
+package com.stockx.livestocktracker.configurations;
 
 
 import com.amazonaws.auth.AWSCredentials;
@@ -6,7 +6,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNSClient;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
