121a5731e80e1bcf0d0553d0ada82a75efe6983f
wanger26 <jwanger@uwo.ca>
2021-11-24 19:58:29 -0500

    Added extra token to distribute the load

--- src/main/java/com/stockx/livestocktracker/adapters/FinnhubAdapter.java
+++ src/main/java/com/stockx/livestocktracker/adapters/FinnhubAdapter.java
@@ -4,8 +4,11 @@ import com.stockx.livestocktracker.Retrieval;
 import com.stockx.livestocktracker.Stock;
 import com.stockx.livestocktracker.SymbolLookupResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
+
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
@@ -16,40 +19,45 @@ public class FinnhubAdapter {
 
     private static final String BASE_URL = "https://finnhub.io/api/v1";
 
-    public Stock getStockByTicker(String ticker) {
-        String url = UriComponentsBuilder.fromUriString(BASE_URL).path("/quote")
-                .queryParam("symbol", ticker)
-                .queryParam("token", "c5q4mpqad3iaqkuehumg").build().toUriString();
+    private static final String API_TOKEN_1 = "c5q4mpqad3iaqkuehumg";
+    private static final String API_TOKEN_2 = "c6fcue2ad3idclgqa9hg";
 
-        return restTemplate.getForObject(url, Stock.class);
+
+    public Stock getStockByTicker(String ticker) {
+        Stock result = null;
+        try {
+            result = restTemplate.getForObject(getUrlForQuote(API_TOKEN_1, ticker), Stock.class);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.TOO_MANY_REQUESTS) {
+                result = restTemplate.getForObject(getUrlForQuote(API_TOKEN_2, ticker), Stock.class);
+            }
+        }
+        return result;
     }
 
     public SymbolLookupResult symbolLookup(String query) {
         String url = UriComponentsBuilder.fromUriString(BASE_URL).path("/search")
                 .queryParam("q", query)
-                .queryParam("token", "c5q4mpqad3iaqkuehumg").build().toUriString();
+                .queryParam("token", API_TOKEN_2).build().toUriString();
 
         return restTemplate.getForObject(url, SymbolLookupResult.class);
     }
 
     //Interval is used by schedules methods in Service file to indicate frequency of returned historical data.
-    public Retrieval getStockHistorical(String ticker, char interval){
+    public Retrieval getStockHistorical(String ticker, char interval) {
         String t1;
         String period;
 
-        if(interval == 'm'){
-            t1 = String.valueOf((Instant.ofEpochSecond(0L).until(Instant.now(), ChronoUnit.SECONDS)) -3660);
+        if (interval == 'm') {
+            t1 = String.valueOf((Instant.ofEpochSecond(0L).until(Instant.now(), ChronoUnit.SECONDS)) - 3660);
             period = "1";
-        }
-        else if(interval == 'h'){
-            t1 = String.valueOf((Instant.ofEpochSecond(0L).until(Instant.now(), ChronoUnit.SECONDS)) -86400);
+        } else if (interval == 'h') {
+            t1 = String.valueOf((Instant.ofEpochSecond(0L).until(Instant.now(), ChronoUnit.SECONDS)) - 86400);
             period = "60";
-        }
-        else if(interval == 'd'){
+        } else if (interval == 'd') {
             t1 = String.valueOf((Instant.ofEpochSecond(0L).until(Instant.now(), ChronoUnit.SECONDS)) - 31449600);
             period = "D";
-        }
-        else{
+        } else {
             return null;
         }
 
@@ -59,9 +67,15 @@ public class FinnhubAdapter {
                 .queryParam("resolution", period)
                 .queryParam("from", t1)
                 .queryParam("to", t2)
-                .queryParam("token", "c5q4mpqad3iaqkuehumg").build().toUriString();
+                .queryParam("token", API_TOKEN_1).build().toUriString();
 
         return restTemplate.getForObject(url, Retrieval.class);
+    }
+
+    private String getUrlForQuote(String apiToken, String ticker) {
+        return UriComponentsBuilder.fromUriString(BASE_URL).path("/quote")
+                .queryParam("symbol", ticker)
+                .queryParam("token", apiToken).build().toUriString();
     }
 
 
