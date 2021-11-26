bdf8be61aaa2228b4b04bc3f3cc9d30658a7e8d8
wanger26 <jwanger@uwo.ca>
2021-11-26 10:49:19 -0500

    Fixed CORS errors

--- src/main/java/com/stockx/livestocktracker/controllers/LiveStockTrackerController.java
+++ src/main/java/com/stockx/livestocktracker/controllers/LiveStockTrackerController.java
@@ -5,11 +5,7 @@ import com.stockx.livestocktracker.SymbolLookupResult;
 import com.stockx.livestocktracker.services.LiveStockTrackerService;
 import com.stockx.livestocktracker.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,16 +14,19 @@ public class LiveStockTrackerController {
     @Autowired
     private LiveStockTrackerService liveStockTrackerService;
 
+    @CrossOrigin(origins = "https://www.stockx.software")
     @GetMapping("/stock-quote")
     public Stock getStockByTicker(@RequestParam("ticker") String ticker) {
         return liveStockTrackerService.getStockByTicker(ticker);
     }
 
+    @CrossOrigin(origins = "https://www.stockx.software")
     @GetMapping("/find-symbol")
     public SymbolLookupResult findSymbol(@RequestParam("query") String query) {
         return liveStockTrackerService.findSymbol(query);
     }
 
+    @CrossOrigin(origins = "https://www.stockx.software")
     @GetMapping("/graphs")
     public Graphs getGraphs(@RequestParam("ticker") String ticker) {
         return liveStockTrackerService.getGraphs(ticker);
