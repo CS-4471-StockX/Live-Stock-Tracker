f340d628003aca3e9cf54ece26c538d292d6f7c1
wanger26 <jwanger@uwo.ca>
2021-12-09 09:39:21 -0500

    Removed creds

--- src/main/java/com/stockx/livestocktracker/configurations/AppConfiguration.java
+++ src/main/java/com/stockx/livestocktracker/configurations/AppConfiguration.java
@@ -2,6 +2,7 @@ package com.stockx.livestocktracker.configurations;
 
 import com.stockx.livestocktracker.HistoricalData;
 import com.stockx.livestocktracker.adapters.FinnhubAdapter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -9,14 +10,19 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfiguration {
 
+    @Value("${finnhub.apiToken1}")
+    private String finnhubApiToken1;
+    @Value("${finnhub.apiToken1}")
+    private String finnhubApiToken2;
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     @Bean
-    public FinnhubAdapter finnhubAdapter() {
-        return new FinnhubAdapter();
+    public FinnhubAdapter finnhubAdapter(RestTemplate restTemplate) {
+        return new FinnhubAdapter(restTemplate, finnhubApiToken1, finnhubApiToken2);
     }
 
     @Bean
